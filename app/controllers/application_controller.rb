966c61369f427c840e032ea1eda9ec3cbaf3fda6
utthao <thaoduyendao98@gmail.com>
2019-12-11 08:39:32 +0700

    chapter_8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  include SessionsHelper
 
   private
 
   def set_locale
   I18n.locale = params[:locale] || I18n.default_locale
   end
+
   def default_url_options
   {locale: I18n.locale}
   end
+  protect_from_forgery with: :exception
 end
